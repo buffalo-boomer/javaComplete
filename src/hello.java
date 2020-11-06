82f4a59bb125ae73cf1500b1b863e9e298f898ff
Hudson Woomer <hudson.t.woomer@gmail.com>
2020-11-06 18:37:36 -0500

    Initial commit

---
+++ src/hello.java
@@ -0,0 +1,10 @@
+public class hello {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, Hudson");
+
+        //Example of initializing and printing an integer variable
+        int myFirstNumber = 5;
+        System.out.println(myFirstNumber);
+    }
+}
